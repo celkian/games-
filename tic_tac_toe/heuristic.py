cc6b636a686a1a0cdda87105ab83ee1e2b866b74
ca3-10 <riseredasthedawn926@gmail.com>
2023-01-25 05:37:06 +0000

    tic tac toe heuristc tree

--- tic_tac_toe/heuristic.py
+++ tic_tac_toe/heuristic.py
@@ -1,5 +1,4 @@
-
-class Node:
+class HeuristicNode:
 
     def __init__(self, game_state): 
         self.game_state = game_state
@@ -7,7 +6,8 @@ class Node:
         self.winner = self.check_win_states()
         self.parents = []
         self.children = []
-        self.minimax_value = None
+        self.heuristic_value = None
+        self.depth = 0 
     
     def upcoming_player(self): 
         upcoming_player = 2
@@ -45,6 +45,39 @@ class Node:
         
         return None
     
+    def calc_heuristic_value(self): 
+        board = self.game_state
+        all_rows = [[self.game_state[0 + 3*i], self.game_state[1 + 3*i], self.game_state[2 + 3*i]] for i in range(0,3)]
+        all_columns = [[self.game_state[0 + i], self.game_state[3 + i], self.game_state[6 + i]] for i in range(0,3)]
+        all_diagonals = [[self.game_state[2], self.game_state[4], self.game_state[6]], [self.game_state[0],self.game_state[4],self.game_state[8]]]
+
+        all_states = all_rows + all_columns + all_diagonals
+
+        winner_counter = 0
+        opponent_counter = 0
+
+        for states in all_states: 
+            state_value = self.check_if_state(states)
+            if state_value == self.upcoming_player: 
+                winner_counter += 1 
+            elif state_value == False: 
+               1 + 1 
+            else: 
+                opponent_counter += 1 
+
+        return ((winner_counter - opponent_counter) / 8)
+        
+    
+    def check_if_state(self, state): 
+        if state[0] == state[1] != 0 and state[2] == 0: 
+            return state[0]
+        elif state[0] == state[2] != 0 and state[1] == 0: 
+            return state[0]
+        elif state[1] == state[2] != 0 and state[0] == 0: 
+            return state[1]
+        else: 
+            return False
+
     def remaining_moves(self): 
     
         avaliable_moves = []
@@ -52,7 +85,6 @@ class Node:
             if self.game_state[i] == 0:
                 avaliable_moves.append(i)
         return avaliable_moves
-
 
 class Queue:
     def __init__(self):
@@ -67,24 +99,25 @@ class Queue:
     def dequeue(self):
         self.items.pop(0)
 
-
-class TicTacToeRecombiningTree: 
-    def __init__(self): 
+class TicTacToeHeuristicTree: 
+    def __init__(self,game_state,ply): 
+        self.ply = ply
+        self.root = HeuristicNode(game_state)
         self.generate_tree()
-        self.assign_minimax_values(self.root)
+        self.assign_heuristic_values(self.root)
     
     def generate_tree(self): 
         self.nodes = {}
-        empty_board = Node([0 for i in range(9)])
-        self.root = empty_board
-        self.nodes[tuple(empty_board.game_state)] = empty_board
+        self.nodes[tuple(self.root.game_state)] = self.root
 
         queue = Queue()
-        queue.enqueue(empty_board)
-    
+        queue.enqueue(self.root)
+
         while len(queue.items) != 0:
 
             current_node = queue.items[0]
+            if current_node.depth >= self.ply: 
+                break
 
             if current_node.winner == None: 
                 avaliable_moves = current_node.remaining_moves()
@@ -99,9 +132,11 @@ class TicTacToeRecombiningTree:
                         new_node = self.nodes[tuple(new_move_board)]
                         current_node.children.append(new_node)
                         new_node.parents.append(current_node)
+                        new_node.depth = current_node.depth + 1
                         continue
-                    
-                    new_node = Node(new_move_board)
+
+                    new_node = HeuristicNode(new_move_board)
+                    new_node.depth = current_node.depth + 1
                     new_node.parents.append(current_node)
                     current_node.children.append(new_node)
                     queue.enqueue(new_node)
@@ -110,43 +145,46 @@ class TicTacToeRecombiningTree:
             queue.dequeue() 
         self.num_nodes = len(self.nodes)
     
-    def assign_minimax_values(self, node):
+        
+    def assign_heuristic_values(self, node):
         
         if node.children == []: 
             if node.winner == 1:
-                node.minimax_value = 1
+                node.heuristic_value = 1
             elif node.winner == 2:
-                node.minimax_value = -1
+                node.heuristic_value = -1
             elif node.winner == 'Tie':
-                node.minimax_value = 0
+                node.heuristic_value = 0
+            else: 
+                node.heuristic_value = node.calc_heuristic_value()
+                
         
         else:
-            children_minimax_values = []
-            #print("a" + str(len(node.children)))
+            children_heuristic_values = []
 
             for child in node.children:
-                self.assign_minimax_values(child)
-                children_minimax_values.append(child.minimax_value)
+                self.assign_heuristic_values(child)
+                children_heuristic_values.append(child.heuristic_value)
 
 
             if node.upcoming_player == 1:
-                node.minimax_value = max(children_minimax_values)
+                node.heuristic_value = max(children_heuristic_values)
             else:
-                node.minimax_value = min(children_minimax_values)
+                node.heuristic_value = min(children_heuristic_values)
         
-        return node.minimax_value
+        return node.heuristic_value
+
     
     
+        
     
-    
+tree = TicTacToeHeuristicTree([0,0,0,0,0,0,0,0,0],6)
+print(tree.num_nodes)
 
+values = []
+for nodes in tree.nodes: 
+    values.append(tree.nodes[nodes].heuristic_value)
 
+value_two = list(set(values))
 
-        
-        
-            
-
-        
-
-tree = TicTacToeRecombiningTree()
-
+print(value_two)

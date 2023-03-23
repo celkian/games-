55958de63a360735aeb383d9c6d76b9fb1899a61
ca3-10 <riseredasthedawn926@gmail.com>
2023-03-23 06:01:33 +0000

    not adding nodes correctly

--- connect_four/heuristic_player.py
+++ connect_four/heuristic_player.py
@@ -1,6 +1,7 @@
 from c4_tree import *
 from game import *
 from random_strat import *
+import random
 
 class HeuristicPlayer:
     def __init__(self,ply): 
@@ -13,11 +14,16 @@ class HeuristicPlayer:
     def choose_move(self, board):
         board_node = self.nodes[tuple([tuple(board[i]) for i in range(6)])]
 
+        if board == [[0,0,0,0,0,0,0] for i in range(6)]: 
+            return 3
        
         self.tree.assign_heuristic_values(board_node)
 
         children_heuristic_values_dict = {tuple([tuple(children.game_state[i]) for i in range(6)]) : children.heuristic_value for children in board_node.children}
-        print(children_heuristic_values_dict)
+        
+        if len(children_heuristic_values_dict) == 0: 
+            return random.choice(board_node.remaining_columns())
+
         if board_node.upcoming_player == 1: 
             best_move_board = list(max(children_heuristic_values_dict, key=children_heuristic_values_dict.get))
         else: 
@@ -31,11 +37,14 @@ class HeuristicPlayer:
                 best_move_index = i 
                 return best_move_index
         
-        self.tree.prune_layer(self.prune_depth)
+        #self.tree.prune_layer(self.prune_depth)
         self.tree.add_layer(self.add_depth)
-        self.prune_depth += 1
+        #self.prune_depth += 1
         self.add_depth += 1
 
 
-game = ConnectFour(HeuristicPlayer(4), RandomStrat())
+
+game = ConnectFour(HeuristicPlayer(6), RandomStrat())
 game.run(True)
+
+#don't think its updating the nodes correctly 
